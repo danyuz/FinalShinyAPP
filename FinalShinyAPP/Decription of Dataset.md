f419e019a59912e8ce982b56dca203b0e209bd69
danyuz <zhang.j5291@gmail.com>
2021-03-17 03:13:27 +0800

    UI Part

---
+++ FinalShinyAPP/Decription of Dataset.md
@@ -0,0 +1,17 @@
+#### Context
+The Ramen Rater is a product review website for the hardcore ramen enthusiast (or "ramenphile"), with over 2500 reviews to date. This dataset is an export of "The Big List" (of reviews), converted to a CSV format.
+
+#### Content
+Each record in the dataset is a single ramen product review. Review numbers are contiguous: more recently reviewed ramen varieties have higher numbers. Brand, Variety (the product name), Country, and Style (Cup? Bowl? Tray?) are pretty self-explanatory. Stars indicate the ramen quality, as assessed by the reviewer, on a 5-point scale; this is the most important column in the dataset!
+
+Note that this dataset does not include the text of the reviews themselves. For that, you should browse through [link](https://www.theramenrater.com/) instead!
+
+#### Acknowledgements
+This dataset is republished as-is from the original BIG LIST on [link](https://www.theramenrater.com/).
+
+#### Inspiration
+- What ingredients or flavors are most commonly advertised on ramen package labels?
+
+- How do ramen ratings compare against ratings for other food products (like, say, wine)?
+
+- How is ramen manufacturing internationally distributed?
--- FinalShinyAPP/app.R
+++ FinalShinyAPP/app.R
@@ -1,69 +1,115 @@
 library(shiny)
-library(shinyjs)
-library(tidyverse)
 library(ggplot2)
-library(Stat2Data)
+library(devtools)
 
-data(Film)
+## packages required to use
+library(tidyverse)
+library(shinythemes)
+library(plotly)
+library(shinyjs)
 
-list_choices <-  unique(Film$Time_code)[!is.na(unique(Film$Time_code))]
-names(list_choices) <- paste(unique(Film$Time_code)[!is.na(unique(Film$Time_code))],sep="")
+data <- read.csv("../ramen-ratings.csv", header = TRUE)
 
+# Replacing unrated data with minimum of ratings
+data$Stars[data$Stars == 'Unrated'] <- min(data$Stars)
+data$Style[data$Stars == ''] <- mode(data$Style)
+
+# preprocessing the top ten rated ramen
+data$year <- substr(data$Top.Ten,1,4)
+data$top_ten <- substr(data$Top.Ten, 5, 10)
+
+for (i in 1:length(data$top_ten)) {
+    if (length(data$top_ten[i]) == 4) {
+        data$top_ten[i] = as.integer(substr(data$top_ten[i],3,5))
+    } else if (length(data$top_ten[i] == 3)) {
+        data$top_ten[i] = as.integer(substr(data$top_ten[i],3,4))
+    }
+}
+data$top_ten[is.na(data$top_ten)] <- 0
+data$year[data$year == ""] <- 0
+data$year[data$year == "\n"] <- 0
+
+data <- data[,-7]
 
 # Define UI for application that draws a histogram
-ui <- navbarPage("First Shiny App",
-                 tabPanel("Description of Dataset",
-                          includeMarkdown("./Description of Dataset Film.md")
-                 ), #  tabPanel
-                 tabPanel("Summary of Dataset Film",
-                          verbatimTextOutput("FilmSummary")
+ui <- navbarPage("Final Shiny App",
+                 tabPanel("Description of Ramen Ratings",
+                          includeMarkdown("Decription of Dataset.md")
                  ),
-                 tabPanel("Histogram of Ratings by length",
-                          fluidPage(
-                              sidebarLayout(sidebarPanel(
-                                  selectInput("select1", label = h3("Rating by Length of Film"), 
-                                              choices = list_choices,
-                                              selected = 1)
-                              ), mainPanel(
-                                  h3("Plot"),
-                                  plotOutput(outputId = "histog", click = "plot_click"))
+                 tabPanel("Summary of Ramen Ratings",
+                          verbatimTextOutput("ramenSummary")
+                 ),
+                 tabPanel("Barplot of Countries and Style",
+                          sidebarLayout(
+                              sidebarPanel(
+                                  h2("Barplot Plot"),
+                                  radioButtons("barSelector", 
+                                              choices = c("Countries", "Styles"), 
+                                              label = h4("Select")                                  )
+                              ), #  sidebarPanel
+                              mainPanel(
+                                  plotOutput(outputId = "plot1")
                               )
-                          )),
-                 tabPanel("Rating by Length of Film",
-                          fluidPage(
-                              sidebarLayout(sidebarPanel(
-                                  selectInput("select2", label = h3("Rating by Length of Film"), 
-                                              choices = list_choices,
-                                              selected = 1)
-                              ), mainPanel(
-                                  h3("Plot"),
-                                  plotOutput(outputId = "plot1", click = "plot_click"))
-                              ))),
-                 tabPanel("Rating by Number of Casts of Film",
-                          fluidPage(plotOutput(outputId = "plot2")
-                          ))
+                          ), # sidebarLayout
+                 ), # tabPanel
+                 tabPanel("Boxplot of Ratings of Ramen according to Country and Style",
+                          sidebarLayout(
+                              sidebarPanel(
+                                  h2("Box Plot"),
+                                  selectInput("plotSelector", 
+                                              choices = unique(data$Country), 
+                                              label = h4("Select Country"),
+                                              selected = sample(data$Country,1)
+                                              ),
+                                  selectInput("plotSelector2", 
+                                              choices = unique(data$Style), 
+                                              label = h4("Select Style"),
+                                              selected = sample(data$Style,1)
+                                  ),
+                                        ), #  sidebarPanel
+                              mainPanel(
+                                  plotOutput(outputId = "plot2")
+                              )
+                          ), # sidebarLayout
+                 ), # tabPanel
+                 tabPanel("Top 10 Ramen Details",
+                          sidebarLayout(
+                              sidebarPanel(
+                                  h2("Decription of Top 10 Ramen"),
+                                  numericInput("descripSelector", 
+                                              value = unique(data$year[data$year != 0]), 
+                                              label = h4("Select year")
+                                  ),
+                                  numericInput("descripSelector2", 
+                                              label = h4("Select Top 10"),
+                                              value = unique(data$top_ten[data$top_ten != 0]))
+                              ), #  sidebarPanel
+                              mainPanel(
+                                  tableOutput(outputId = "descrip")
+                              )
+                          ), # sidebarLayout
+                 ),
+                 tabPanel("Top 10 Ramen according to Rating",
+                          sidebarLayout(
+                              sidebarPanel(
+                                  h2("Top 10 Ramen"),
+                                  checkboxGroupInput("toptenSelector", 
+                                                    choices = data$year[data$year != 0], 
+                                                    label = h4("Select year")
+                                  ),
+                              ), #  sidebarPanel
+                              mainPanel(
+                                  plotOutput(outputId = "topten")
+                              )
+                          ), # sidebarLayout
+                 )
 ) # navbarPage
 
-col_scale <- scale_colour_discrete(limits = list_choices)
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    output$FilmSummary<-renderPrint(summary(Film))
-    
-    output$plot1 <- renderPlot({
-        ggplot(Film %>% filter(Time_code == input$select2),
-               aes(x=Rating)) +
-            col_scale +
-            geom_density()
-    })
-    
-    output$plot2 <- renderPlot({
-        ggplot(Film)+geom_point(aes(Cast, Rating))
-    })
-    
-    output$histog <- renderPlot(hist(Film %>%filter(Time_code == input$select1) %>% dplyr::select(Rating) %>% unlist(),main="Histogram of Ratings"))
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
